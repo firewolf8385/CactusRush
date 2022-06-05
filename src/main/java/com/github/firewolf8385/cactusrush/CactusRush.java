391612d4f6a9d16fd0c5e14f209f7d98142fa66c
colby <colbykrawczyk10@gmail.com>
2022-06-05 01:21:59 -0400

    Require PlaceholderAPI for future configuration

--- src/main/java/com/github/firewolf8385/cactusrush/CactusRush.java
+++ src/main/java/com/github/firewolf8385/cactusrush/CactusRush.java
@@ -1,6 +1,7 @@
 package com.github.firewolf8385.cactusrush;
 
 import com.github.firewolf8385.cactusrush.player.CactusPlayerManager;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CactusRush extends JavaPlugin {
@@ -10,6 +11,16 @@ public final class CactusRush extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         cactusPlayerManager = new CactusPlayerManager(this);
+
+        // If PlaceholderAPI is installed, enables placeholders
+        if(Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            new Placeholders(this).register();
+        }
+        else {
+            // If not, logs a warning and disables the plugin
+            Bukkit.getLogger().warning("CactusRush requires PlaceholderAPI to be installed.");
+            getServer().getPluginManager().disablePlugin(this);
+        }
     }
 
     /**
