f32177cf4dd3a1b3b69509e25f4c678410704cd6
colby <colbykrawczyk10@gmail.com>
2022-12-19 14:44:01 -0500

    Add wins to the lobby scoreboard

--- src/main/java/com/github/firewolf8385/cactusrush/LobbyScoreboard.java
+++ src/main/java/com/github/firewolf8385/cactusrush/LobbyScoreboard.java
@@ -45,14 +45,15 @@ public class LobbyScoreboard extends CustomScoreboard {
 
         // Sets up the scoreboard.
         helper.setTitle("&a&lCACTUS RUSH");
-        helper.setSlot(10, "&7" + DateUtils.currentDateToString());
-        helper.setSlot(9, "");
-        helper.setSlot(8, "&aLevel: " + LevelUtils.getFormattedLevel(cactusPlayer.getLevel()));
-        helper.setSlot(7, "");
-        helper.setSlot(6, "&aProgress: &7" + LevelUtils.getFormattedExperience(cactusPlayer.getExperience()) + "&7/&a" + LevelUtils.getFormattedRequiredExperience(cactusPlayer.getLevel()));
-        helper.setSlot(5, " " + LevelUtils.getSmallLevelBar(cactusPlayer.getExperience(), cactusPlayer.getLevel()));
-        helper.setSlot(4, "");
-        helper.setSlot(3, "&aCoins: " + "&6" + cactusPlayer.getCoins());
+        helper.setSlot(11, "&7" + DateUtils.currentDateToString());
+        helper.setSlot(10, "");
+        helper.setSlot(9, "&aLevel: " + LevelUtils.getFormattedLevel(cactusPlayer.getLevel()));
+        helper.setSlot(8, "");
+        helper.setSlot(7, "&aProgress: &7" + LevelUtils.getFormattedExperience(cactusPlayer.getExperience()) + "&7/&a" + LevelUtils.getFormattedRequiredExperience(cactusPlayer.getLevel()));
+        helper.setSlot(6, " " + LevelUtils.getSmallLevelBar(cactusPlayer.getExperience(), cactusPlayer.getLevel()));
+        helper.setSlot(5, "");
+        helper.setSlot(4, "&aCoins: " + "&6" + cactusPlayer.getCoins());
+        helper.setSlot(3, "&aWins: &f" + cactusPlayer.getWins());
         helper.setSlot(2, "");
         helper.setSlot(1, "&ajadedmc.net");
     }
