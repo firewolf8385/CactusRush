c6ee2c7f6f74b365ab772ad186d78943ec48e5cc
colby <colbykrawczyk10@gmail.com>
2023-09-01 16:21:21 -0400

    Make ability descriptions use the cooldown variable

--- src/main/java/com/github/firewolf8385/cactusrush/game/ability/abilities/BlindAbility.java
+++ src/main/java/com/github/firewolf8385/cactusrush/game/ability/abilities/BlindAbility.java
@@ -1,3 +1,27 @@
+/*
+ * This file is part of JadedChat, licensed under the MIT License.
+ *
+ *  Copyright (c) JadedMC
+ *  Copyright (c) contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
 package com.github.firewolf8385.cactusrush.game.ability.abilities;
 
 import com.github.firewolf8385.cactusrush.game.Game;
@@ -18,6 +42,11 @@ import java.util.List;
 import java.util.Map;
 
 public class BlindAbility extends Ability {
+
+    /**
+     * Creates the Blind Ability, which blinds the nearest player when used.
+     * @param plugin Instance of the plugin.
+     */
     public BlindAbility(Plugin plugin) {
         super(plugin, "blind", "&8&lBlind", 45);
     }
@@ -30,7 +59,7 @@ public class BlindAbility extends Ability {
                 .addLore("&7Gives the closest opponent blindness")
                 .addLore("&7for 5 seconds!")
                 .addLore("")
-                .addLore("&eCooldown: 45 seconds");
+                .addLore("&eCooldown: " + this.cooldown() + " seconds.");
 
         return builder.build();
     }
--- src/main/java/com/github/firewolf8385/cactusrush/game/ability/abilities/BreakerAbility.java
+++ src/main/java/com/github/firewolf8385/cactusrush/game/ability/abilities/BreakerAbility.java
@@ -1,3 +1,27 @@
+/*
+ * This file is part of JadedChat, licensed under the MIT License.
+ *
+ *  Copyright (c) JadedMC
+ *  Copyright (c) contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
 package com.github.firewolf8385.cactusrush.game.ability.abilities;
 
 import com.github.firewolf8385.cactusrush.game.Game;
@@ -24,7 +48,7 @@ public class BreakerAbility extends Ability {
                 .addLore("&7Make yourself instantly break cactus")
                 .addLore("&7for 3 seconds!")
                 .addLore("")
-                .addLore("&eCooldown: 45 seconds");
+                .addLore("&eCooldown: " + this.cooldown() + " seconds.");
 
         return builder.build();
     }
