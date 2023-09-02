b84f93ce9077ff45ba0ba23ed52fde944862adad
colby <colbykrawczyk10@gmail.com>
2023-09-01 21:33:22 -0400

    Delete legacy code

---
+++ src/main/java/net/jadedmc/cactusrush/CactusRushPlugin.java
@@ -0,0 +1,12 @@
+package net.jadedmc.cactusrush;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CactusRushPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
